ec309e32a90aa9c0096d7495d4695de5aa90750d
maciel <161737831+gabrielmaciel09@users.noreply.github.com>
2024-10-03 09:20:48 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+// fantasia, aventura, drama
+
+// a viagem de chihiro, LIVRE, fantasia, aventura
+// paddington, LIVRE, fantasia, aventura
+// as aventuras de pi, 10, drama, fantasia, aventura
+// depois da chuva, 10, drama
+// guardiões da galáxia, 12, fantasia, aventura
+// ladrões de bicicleta, 12, drama
+// o menino que descobriu o vento, 14, drama
+let campoIdade;
+
+function setup() {
+  createCanvas(400, 400);
+  campoIdade = createInput("15");
+}
+
+function draw() {
+  background(220);
+  let idade = 15; // exemplo de idade
+  let recomendacao = geraRecomendacao(idade);
+  text(recomendacao, width / 2, height / 2);
+}
+
+function geraRecomendacao(idade) {
+  if (idade >= 10) {
+    if (idade >= 14) {
+      return "O menino que descobriu o vento";
+    } else {
+      return "As aventuras de Pi";
+    }
+  } else {
+    return "A viagem de Chihiro";
+  }
+}
